f0275e80bbedf597d9a506c646e739af01b2f05b
mdataullahansary <mdataullahansari2003@gmail.com>
2025-03-15 14:30:25 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('bookNowButton').addEventListener('click', function() {
+          window.location.href = 'booking.html';
+        });
+      
+
+document.getElementById('home').addEventListener('click', function() {
+            window.location.href = 'index.html';
+          });
+        
+  
