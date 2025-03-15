6451b561d551540b38fdfa6f494e5159bc5f78bc
mdataullahansary <mdataullahansari2003@gmail.com>
2025-03-15 15:19:58 +0530

    Added favicon and fixed file path

--- script.js
+++ script.js
@@ -6,5 +6,11 @@ document.getElementById('bookNowButton').addEventListener('click', function() {
 document.getElementById('home').addEventListener('click', function() {
             window.location.href = 'index.html';
           });
-        
-  
+
+document.getElementById('tracking').addEventListener('click', function() {
+            window.location.href = 'live_tracking.html';
+          });
+
+document.getElementById('contacts').addEventListener('click', function() {
+            window.location.href = 'contacts.html';
+          });
